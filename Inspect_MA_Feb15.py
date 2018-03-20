0729115aea581d88c6e74811cac799dc48b9d63e
Lynda Anne <lsefton62@gmail.com>
2018-03-20 15:01:14 -0400

    Sliced Boston

--- Inspect_MA_Feb15.py
+++ Inspect_MA_Feb15.py
@@ -6,23 +6,30 @@ import numpy as np
 #wth_data = pd.read_csv('CA_weather_data_janfeb17.csv', parse_dates=['DATE'], index_col=['DATE'])
 wth_data = pd.read_csv('MA_Feb15.csv', parse_dates=['DATE'])
 #inspect data
-print(wth_data.head())
-print(wth_data.tail())
-print(wth_data.index)
+#print(wth_data.head())
+#print(wth_data.tail())
+#print(wth_data.index)
 print(wth_data.columns)
 #delete unneeded column
 del wth_data['STATION']
-print(wth_data.head())
+#print(wth_data.head())
 #index data and sort data based on town
 wth_data = wth_data.set_index(['NAME', 'DATE'])
 sorted_data = wth_data.sort_index()
-print(sorted_data.head())
+
+loc=0
+for index, row in sorted_data.iterrows():
+    print(sorted_data[loc:loc+50])
+    if loc <= 3500:
+        loc = loc + 50
+    else: break
+#print(sorted_data.head())
 #create precip df and fill NaN for prcp and snow data with 0
 sorted_precip = sorted_data[['PRCP', 'SNOW']].fillna(0)
-print(sorted_precip.head(50))
+#print(sorted_precip.head(50))
 #create temp df
 sorted_temp = sorted_data[['TMAX', 'TMIN']]
-print(sorted_temp.tail(50))
+#print(sorted_temp.tail(50))
 #Histogram of precipitation
 precip= sorted_precip['PRCP']
 plt.xlabel('Inches of Precipiation')
@@ -30,7 +37,7 @@ plt.ylabel('Number of days')
 plt.title('MA Precipiation Feb 2015')
 precip.plot(y='PRCP', kind='hist', bins=10)
 plt.savefig('precip_hist.pdf')
-plt.show()
+#plt.show()
 #Histogram of Snowfall
 snow= sorted_precip['SNOW']
 plt.xlabel('Inches of Snow')
@@ -38,7 +45,7 @@ plt.ylabel('Number of days')
 plt.title('MA Snowfall totals Feb 2015')
 snow.plot(y='SNOW', kind='hist', bins=10)
 plt.savefig('snow_hist.pdf')
-plt.show()
+#plt.show()
 #Histogram of Maximum Temperatures
 tmax = sorted_temp['TMAX']
 tmax.plot(y='TMAX', kind='hist', bins=20)
@@ -47,7 +54,7 @@ plt.xlabel('Degrees F')
 plt.ylabel('Number of days')
 plt.title('MA Maximum Temperatures Feb 2015')
 plt.savefig('tmax_hist.pdf')
-plt.show()
+#plt.show()
 #Histogram of Minimum Temperatures
 tmin = sorted_data['TMIN']
 plt.xlabel('Degrees F')
@@ -55,10 +62,10 @@ plt.ylabel('Number of days')
 plt.title('MA Minimum Temperatures Feb 2015')
 tmin.plot(y='TMIN', kind='hist', bins=20)
 plt.savefig('tmin_hist.pdf')
-plt.show()
+#plt.show()
 
 #Slice based on town(name)
-town1 =sorted_data.loc[('ACTON 1.3 SW, MA US'), :]
+town1 =sorted_data.loc[('BOSTON, MA US', slice(None)), :]
 print(town1)
 
 #wth_data_precip = pd.melt(frame=wth_data, id_vars=['DATE', 'NAME'], value_vars=['PRCP', 'SNOW'],  var_name='Type', value_name='Reading')
